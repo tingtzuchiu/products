efe7a55a0b8e9a7e078baceab969626ff07a188f
Claire Chiu <tingtzuchiu@gmail.com>
2019-07-06 11:21:01 -0700

    import os

--- products.py
+++ products.py
@@ -1,15 +1,21 @@
-#讀取檔案
+import os #operating system 
+
 products = []
-with open("products1.csv","r", encoding = "utf-8") as f:
-	for line in f:
-		if "商品,價格" in line:
-			continue #繼續下一個迴圈，不會執行下面的程式
-		name, price= line.strip().split(",")
+if os.path.isfile("products1.csv"):
+	print("Yeah!找到檔案了")
+	#讀取檔案
+	with open("products1.csv","r", encoding = "utf-8") as f:
+		for line in f:
+			if "商品,價格" in line:
+				continue #繼續下一個迴圈，不會執行下面的程式
+			name, price= line.strip().split(",")
 		#name = s[0] 這兩行 變成上面一行
 		#price = s[1]
 		#strip.() = 去掉\n。split分割，（）裡面寫用什麼切割
-		products.append([name, price])
-print(products)
+			products.append([name, price])
+	print(products)
+else:
+	print("找不到檔案")
 
 #讓使用者輸入 
 products = []
